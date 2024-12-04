7929ab3e3a1a0b3bf7ea7c92e69454bef411b7c5
Sushant Shendage <sushantshendage44@gmail.com>
2024-12-04 22:19:28 +0530

    update

--- react-landing-page/src/componenets/NavDiv.jsx
+++ react-landing-page/src/componenets/NavDiv.jsx
@@ -4,7 +4,7 @@
    return (
      <div id="NavDivId">
       <ul  id="listId">
-        <li><img src="https://www.bing.com/ck/a?!&&p=658e73f2705f345d14fd0e96b0830fbed980369dc517cfe42899c85f39e74073JmltdHM9MTczMzE4NDAwMA&ptn=3&ver=2&hsh=4&fclid=3cacbc61-862f-6884-08a9-af7787876936&u=a1L2ltYWdlcy9zZWFyY2g_cT13ZWJpbmFyJTIwaWNvbnMmRk9STT1JUUZSQkEmaWQ9NTYyNEZCQzZBRTI5Qzk4NkFEQUM5NUU0NTdDNzZCM0FBQTZDMjRFMw&ntb=1"></img></li>
+        <li><img src="https://tse4.mm.bing.net/th?id=OIP.BQbGf0ukZsYAppcB9hinfQHaHa&pid=Api&P=0&h=220" alt='iconNotLoaded' id="img1Id"></img></li>
         <li>compaign</li>
         <li>About</li>
         <li>contact</li>
