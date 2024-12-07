06c26427114331b1c1658acd39ce3431b6929371
Sushant Shendage <sushantshendage44@gmail.com>
2024-12-07 10:04:33 +0530

    update

--- project1/src/component/JSXExpressionrules/JSXExpressionRule3.jsx
+++ project1/src/component/JSXExpressionrules/JSXExpressionRule3.jsx
@@ -1,16 +1,21 @@
 import React,{Fragment} from 'react'
 
 function JSXExpressionRule3() {
-    let names=["user1","user2","user3","user4","user5","user6",]
+    let names=["ram","shyam","seeta","geeta","meena","user6"]
   return (
     //JSXExpressionRule3
     // <div>JSXExpressionRule3</div>
-    //! we can't use all looping stattement inside jsx expression .We can use only the higher-order functions but  all higher order functions are not usefull only those higher order functions are useful that can return the jsx
+    //! we can't use all looping stattement inside jsx expression . We can use only the higher-order functions but  all higher order functions are not usefull only those higher order functions are useful that can return anything/value the jsx
+    //! mainly the map(),filter() and reduce()
+    
+    // <div>
+    //     <p>the names of students are {names.map((ele,i)=><Fragment key={i}>{ele+` `}</Fragment>)}</p>
+    // </div>
+    //here we are displying only those elements which satisfies condition i.e elements eanding with a
 
-    <div>
-        <p>the names of students are {names.map((ele,i)=><Fragment key={i}>{ele+` `}</Fragment>)}</p>
-    </div>
-
+<div>
+<p>the names of students are {names.map((ele,i)=><Fragment key={i}>{ele.endsWith('a') && ele+` `}</Fragment>)}</p>
+</div>
 
   )
 }
--- project1/src/component/jsxRules/Jsxrule6.jsx
+++ project1/src/component/jsxRules/Jsxrule6.jsx
@@ -5,15 +5,14 @@ import JSXExpressionRule3 from '../JSXExpressionrules/JSXExpressionRule3';
 
 function Jsxrule6() {
     let price=99;
-  return (
-    <div>
+  return <div>
         {/* <h1>{price}</h1> */}
         {/* <JSXExpressionRule1></JSXExpressionRule1> */}
         {/* <JSXExprsiionRule2/> */}
         <JSXExpressionRule3 />
     </div>
      
-  )
+  
 }
 
 export default Jsxrule6
